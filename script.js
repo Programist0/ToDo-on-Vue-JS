46fb4bb396077f9007e04e067b82bed15e93d67a
Programist0 <49829520+Programist0@users.noreply.github.com>
2021-06-25 12:43:09 +0300

    First commit
    Stable version 1.8

---
+++ script.js
@@ -0,0 +1,70 @@
+
+const App = {
+	data(){
+		return{
+			counter: 1,
+			title: 'What to do today?',
+			placeholderString: 'Add your first note!',
+			inputValue: '',
+			notes: [],
+			importantNotes: [],
+			timeOfNote: [],
+			info: '',
+			important: false,
+			importantNotes: [],
+			time: '12:00',
+			first: false
+		}
+	},
+	methods: {
+		inputChangeHandler(event){
+			this.inputValue = event.target.value;
+		},
+		addNewNote(){
+			if(this.inputValue !== ''){
+				console.log(this.time);
+				if(this.first){
+					this.notes.unshift(this.inputValue);
+					this.importantNotes.unshift(this.important);
+					this.timeOfNote.unshift(this.time);
+				} else{
+					this.notes.push(this.inputValue);
+					this.importantNotes.push(this.important);
+					this.timeOfNote.push(this.time);
+				}
+				this.time = '12:00';
+				this.important = false;
+				this.first = false;
+				this.inputValue = '';
+			}
+		},
+		deleteNote(index, event){
+			this.notes.splice(index, 1);
+			this.importantNotes.splice(index, 1);
+			this.timeOfNote.splice(index, 1);
+		},
+		toUpperCase(item){
+			return item.toUpperCase();
+		}
+	},
+	computed:{
+		doubleCountComputed(){
+			return this.notes.length = 2;
+		}
+	},
+	watch:{
+		inputValue(value){
+			if(value.length < 50) {
+				this.info = '';
+			}
+			if(value.length > 50){
+				this.info = 'Max length of note!';
+				this.inputValue = value.substring(0, value.length - 1);
+			}
+		}
+	}
+} 
+
+const app = Vue.createApp(App)
+
+app.mount('#app')
